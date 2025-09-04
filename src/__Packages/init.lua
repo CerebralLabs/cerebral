633afcba8367a7e13945ede5e21445e3795b3724
Rueben Engelhart <engelhartrueben@gmail.com>
2025-09-04 19:51:28 -0400

    (chore): __Package comment

--- src/__Packages/init.lua
+++ src/__Packages/init.lua
@@ -1,7 +1,13 @@
+--[[
+__Packages is not really a package directory. It is no like node_modules
+to Node.js. Instead, this directory harbors variours code snippets I come
+across in the Luau community (proper license of course).
+]]
+
 local Signal = require(script.Signal)
 
 local Packages = {
-    Signal = Signal
+    Signal = Signal -- Handles Luau event signals
 }
 
 return Packages
