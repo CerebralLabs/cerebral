f3887761f647be0bcd93ada16276010946746c4a
Rueben Engelhart <engelhartrueben@gmail.com>
2025-09-04 20:03:31 -0400

    (DefaultModules): Add basic String methods for the String DefaultModule

--- src/DefaultModules/String.lua
+++ src/DefaultModules/String.lua
@@ -1,6 +1,13 @@
 local __Types = require(script.Parent.Parent.__Types)
 
-type Module = {}
+type Module = {
+	Get: () -> string,
+	Set: (value: string) -> string,
+	GetLen: () -> number,
+	Lower: () -> string,
+	Upper: () -> string,
+	Concat: (value: string) -> string
+}
 
 export type StringModule = __Types.Attribute<Module, string> 
 
@@ -24,6 +31,34 @@ function String.New(value: string)
 	setmetatable(self, string)
 
 	return self
+end
+
+function String:Get(): string
+	return self.value
+end
+
+function String:Set(value: string): string
+	self.value = value
+	return value
+end
+
+function String:GetLen(): number
+	return string.len(self.value)
+end
+
+function String:Upper(): string
+	self.value = string.upper(self.value)
+	return self.value
+end
+
+function String:Lower(): string
+	self.value = string.lower(self.value)
+	return self.value
+end
+
+function String:Concat(value: string): string
+	self.value = self.value .. value
+	return self.value
 end
 
 return String
